62c22d8a1af97ce4979337b10e0dd1fb8bba8e78
Luke Pascale <lpluke5@gmail.com>
2022-02-02 09:22:39 -0700

    feat: Add add_student_name method to school class.

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,17 @@
 # ./lib/school.rb
 class School
-  attr_reader :start_time, :hours_in_school_day, :student_names
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
+  end
+
+  def add_student_name(student_name)
+    @student_names << student_name 
   end
 
 end
