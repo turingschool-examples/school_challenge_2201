8627e50df1ef9f886f46a36e659b2ae50bbbe505
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-02 10:13:04 -0700

    iterations two, three, four

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,36 @@
- #./lib/school.rb
- class School
-   attr_reader :start_time,
-               :hours_in_school_day,
-               :student_names
+# ./lib/school.rb
+class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
 
-   def initialize(start_time, hours_in_school_day)
-     @start_time = start_time
-     @hours_in_school_day =  hours_in_school_day
-     @student_names = []
-   end
- end
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name(student_name)
+    @student_names << student_name
+  end
+
+  def end_time
+    (start_time.to_i + @hours_in_school_day).to_s + ':00'
+  end
+
+  def is_full_time?
+    @hours_in_school_day > 4
+  end
+
+  def standard_student_names
+    @student_names.map do |student_name|
+      student_name.capitalize
+    end
+  end
+
+  def convert_end_time_to_clock_time
+    if end_time.to_i > 12
+      (end_time.to_i - 12).to_s + (":00")
+    end
+  end
+end
--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe School do
   end
 
   context 'Iteration 2' do
-    xit 'can add student names' do
+    it 'can add student names' do
       school = School.new('9:00', 7)
 
       school.add_student_name('Aurora')
@@ -40,7 +40,7 @@ RSpec.describe School do
       expect(school.student_names).to eq(['Aurora', 'tim', 'megan'])
     end
 
-    xit 'can calculate end time' do
+    it 'can calculate end time' do
       school_1 = School.new('9:00', 7)
       school_2 = School.new('9:00', 3)
 
@@ -51,9 +51,33 @@ RSpec.describe School do
 
   context 'Iteration 3' do
     # Add your own tests that follow the interaction pattern for Iteration 3
+    describe 'is_it_full_time?' do
+      it 'can tell if a school is full time' do
+        school = School.new('9:00', 7)
+        school.add_student_name('Aurora')
+        school.add_student_name('tim')
+        school.add_student_name('megan')
+        expect(school.is_full_time?).to eq(true)
+      end
+    end
+    describe 'standard_student_names' do
+      it 'can return a list of capitalized student names' do
+        school = School.new('9:00', 7)
+        school.add_student_name('Aurora')
+        school.add_student_name('tim')
+        school.add_student_name('megan')
+        expect(school.standard_student_names).to eq(['Aurora', 'Tim', 'Megan'])
+      end
+    end
   end
 
   context 'Iteration 4' do
     # Add your own tests that follow the interaction pattern for Iteration 4
+    describe 'convert_end_time_to_clock_time' do
+      it 'changes format of end time to a 12 hour format' do
+        school = School.new('9:00', 7)
+        expect(school.convert_end_time_to_clock_time).to eq("4:00")
+      end
+    end
   end
 end
