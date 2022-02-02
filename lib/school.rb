9e9cf67e28fa67a749131c8d71caad821aaa5086
alex randolph <alexgeorgerandolph@gmail.com>
2022-02-02 09:51:57 -0700

    Initial Commit & Complete Iteration 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# ./lib/school.rb
+require './lib/school.rb'
+
+class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
+  def initialize(start_time, hours_in_school_day)
+    @hours_in_school_day = hours_in_school_day
+    @start_time = start_time
+    @student_names = []
+  end
+
+
+end
+
+
+#  require './lib/school'
+#
+#  school = School.new('9:00', 7)
+#  school.start_time
+#  school.hours_in_school_day
+#  school.student_names
+#
+#
