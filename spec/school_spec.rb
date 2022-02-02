750b72dc724df20694cc83af06ed7d05403a8666
Will Medders <wmedders@gmail.com>
2022-02-02 11:24:20 -0500

    finish iteration 1

--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -10,19 +10,19 @@ RSpec.describe School do
       expect(school).to be_a(School)
     end
 
-    xit 'has a start time' do
+    it 'has a start time' do
       school = School.new('9:00', 7)
 
       expect(school.start_time).to eq('9:00')
     end
 
-    xit 'has hours in a school day' do
+    it 'has hours in a school day' do
       school = School.new('9:00', 7)
 
       expect(school.hours_in_school_day).to eq(7)
     end
 
-    xit 'starts with no student names' do
+    it 'starts with no student names' do
       school = School.new('9:00', 7)
 
       expect(school.student_names).to eq([])
