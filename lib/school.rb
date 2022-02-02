d1742d9ab14421489ed1e9cb702b8adf99de31e3
Kim G <xiaole.guo@yahoo.com>
2022-02-02 09:28:34 -0700

    Iteration 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,10 @@
 # ./lib/school.rb
+class School
+  attr_reader :start_time, :day, :student_names
+  def initialize(first, second)
+    @start_time = first
+    @day = second
+    @student_names = []
+  end
+
+end
