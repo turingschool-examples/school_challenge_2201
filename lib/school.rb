321f6ec5b2dfa8e40fffbae86421e896379c68dd
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-02 08:55:40 -0800

    full time method and standard student names method to pass iteration 3 tests

--- lib/school.rb
+++ lib/school.rb
@@ -13,4 +13,23 @@ class School
     @student_names << names
   end
 
+  def end_time
+    end_of_school = @start_time.to_f + @hours_in_school_day.to_f
+      end_of_school.to_s
+  end
+
+  def is_full_time?
+    if @hours_in_school_day > 4
+      true
+    end
+  end
+
+  def standard_student_names
+    capitalized_names = []
+    @student_names.each do |name|
+      capitalized_names << name.capitalize
+    end
+    capitalized_names
+  end
+
 end
--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -49,9 +49,21 @@ RSpec.describe School do
     end
   end
 
-  context 'Iteration 3' do
-    # Add your own tests that follow the interaction pattern for Iteration 3
+  ## ITERATION 3 ##
+
+   it 'is school full time?' do
+    school = School.new('9:00', 7)
+    expect(school.is_full_time?).to eq(true)
   end
+
+  it 'capitalized student names' do
+    school = School.new('9:00', 7)
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+    expect(school.standard_student_names).to eq (["Aurora", "Tim", "Megan"])
+  end 
+
 
   context 'Iteration 4' do
     # Add your own tests that follow the interaction pattern for Iteration 4
