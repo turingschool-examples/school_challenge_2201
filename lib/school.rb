45af03a77cc361ae8f2776bfdc1bebc94096e619
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-02 09:58:41 -0700

    Initial commit + completed challenge

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # ./lib/school.rb
+class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
+  def initialize(start_time, hours_in_school_day, student_names = [])
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = student_names
+  end
+  def add_student_name(name)
+    @student_names << name
+  end
+  def end_time
+  return "#{start_time.to_i + hours_in_school_day}:00"
+  end
+  def is_full_time?
+    if hours_in_school_day >= 4
+      return true
+    else
+      return false
+    end
+  end
+  def standard_student_names
+    standard_names = []
+    @student_names.each do |name|
+      standard_names << name.capitalize!
+    end
+  end
+  def convert_end_time_to_clock_time
+    end_time = start_time.to_i + hours_in_school_day
+    if end_time > 12
+      return "#{(end_time - 12).to_s}:00PM"
+    else
+      return "#{end_time}:00AM"
+    end
+  end
+end
