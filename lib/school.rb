f771816209f136ffe21562a5893218f5a8cb4a60
Sherman Au <shermau13@gmail.com>
2022-02-02 11:08:31 -0700

    Fix end time issue

--- lib/school.rb
+++ lib/school.rb
@@ -9,17 +9,20 @@ attr_reader :start_time,
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
-    @end_time = end_time.to_s
+    @end_time = end_time
   end
 
   def add_student_name(student_name)
     @student_names << student_name
   end
 
-  def calculate_end_time(end_time)
-     start_time.to_i + hours_in_school_day << end_time
+  def calculate_end_time
+     start_time.to_i + hours_in_school_day
   end
 
+  def end_time
+    calculate_end_time.to_s + ":00"
+  end
 end
 
 school = School.new('9:00', 7)
