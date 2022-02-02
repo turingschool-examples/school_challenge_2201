2224d057655ccf2159976df73c949aaa31c0096e
Joseph Senecal <jasenecal2@gmail.com>
2022-02-02 09:34:14 -0700

    Complete iteration 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,9 @@
 # ./lib/school.rb
+class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
+def initialize (time_start, duration)
+  @start_time = time_start
+  @hours_in_school_day = duration
+  @student_names = []
+end
+end
