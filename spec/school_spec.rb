ab9530c41b3dc519accb20b8dc9507483bcecdbb
Sherman Au <shermau13@gmail.com>
2022-02-02 10:06:23 -0700

    Add student name

--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe School do
       expect(school.hours_in_school_day).to eq(7)
     end
 
-    xit 'starts with no student names' do
+    it 'starts with no student names' do
       school = School.new('9:00', 7)
 
       expect(school.student_names).to eq([])
@@ -30,7 +30,7 @@ RSpec.describe School do
   end
 
   context 'Iteration 2' do
-    xit 'can add student names' do
+    it 'can add student names' do
       school = School.new('9:00', 7)
 
       school.add_student_name('Aurora')
