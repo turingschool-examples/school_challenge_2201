e58a6716608addf0a6ac8a7ee6b5d49fa2172913
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-02 10:49:51 -0700

    Complete as far as I can

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,19 @@
 # ./lib/school.rb
 class School
-  attr_reader :hours_in_school_day, :start_time, :student_names, :name
+  attr_reader :hours_in_school_day, :start_time, :student_names, :end_time
+
   def initialize(start_time, hours_in_school_day)
     @hours_in_school_day = hours_in_school_day
     @start_time          = start_time
-  end
-
-  def student_names; []
-    @student_names = []
+    @student_names       = []
   end
 
   def add_student_name(name)
-    @name = name
+    @student_names << name
+  end
+
+  def end_time
+   puts "@start_time.to_i + @hours_in_school_day) << ':00'"
+
   end
 end
