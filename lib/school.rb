c48049f6247f85e531f57082e9f7c7e5a0912e2c
Sherman Au <shermau13@gmail.com>
2022-02-02 09:59:19 -0700

    Found syntax error in attr reader

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,13 @@
 # ./lib/school.rb
 class School
-attr_reader :start_time
+attr_reader :start_time,
             :hours_in_school_day
 
   def initialize (start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
-  end  
+  end
+
 end
 
 school = School.new('9:00', 7)
