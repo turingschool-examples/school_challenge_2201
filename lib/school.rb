6c5181635d2af88c3d4a88976a61c4cf9298fb75
reuben <creubend@gmail.com>
2022-02-02 10:11:02 -0700

    Initial commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# ./lib/school.rb
+#./lib/school.rb
+
+class School 
+  attr_reader  :hours_in_a_school_day, :start_time, :student_names
+  
+  def initialize(start, hours)
+    @hours_in_a_school_day = hours
+    @start_time = start
+    @student_names = []
+    
+    
+    def add_student_name(name)
+      @student_names << name
+    end
+    
+    def end_time
+      start_time + hours_in_a_school_day
+    end 
+    
+    
+  end
+  
+end 
