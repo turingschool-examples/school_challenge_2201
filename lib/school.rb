026f0a4f95bb434513fad7274a5636a95ff2b6e2
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-02 10:04:31 -0700

    Initial commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # ./lib/school.rb
+class School
+  attr_accessor :start_time, :hours_in_school_day, :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name(name)
+    student_names << name
+  end
+
+  def end_time
+    end_time_int = start_time.to_i + hours_in_school_day
+    p "#{end_time_int}:00"
+  end
+end
