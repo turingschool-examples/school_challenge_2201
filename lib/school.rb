66b2d18b4c0817fbc068d72d2758affa4543c527
James Harkins <jh.harkins@protonmail.com>
2022-02-02 09:47:54 -0700

    Pass iteration 2 tests

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# ./lib/school.rb
+class School
+
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+    @end_time = @end_time
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    @start_time = @start_time[0]
+    @start_time = @start_time.to_i
+    @end_time = @start_time + @hours_in_school_day
+    @end_time = @end_time.to_s
+    @end_time = @end_time + ":00"
+    @end_time
+  end
+
+end
