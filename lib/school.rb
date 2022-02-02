adace247241a6656cc8610d0c12a4602853efe66
John Hennerich <john.hennerich@gmail.com>
2022-02-02 10:30:21 -0700

    adding iteration 1 and 2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # ./lib/school.rb
+require 'time'
+
+class School
+
+  attr_reader :start_time, :hours_in_school_day, :student_names
+  def initialize(start_time, hours_in_school_day, student_names = [])
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = student_names
+  end
+
+  def add_student_name(name)
+    @student_names.push(name)
+    return @student_names
+  end
+
+  def end_time
+    end_time = (start_time.to_i + hours_in_school_day).to_s + ":00"
+#    p end_time
+    return end_time
+  #  hours_as_string = hours_in_school_day.to_s
+  #  end_time = (Time.parse(start_time) + Time.parse(hours_as_string))
+   # return Time.parse(end_time)
+  end
+end
