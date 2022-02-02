710fd98450e1489884004e99509b83471a622772
“Brad <hutch13531@gmail.com>
2022-02-02 16:15:53 -0500

    Update

--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -10,19 +10,19 @@ RSpec.describe School do
       expect(school).to be_a(School)
     end
 
-    xit 'has a start time' do
+    it 'has a start time' do
       school = School.new('9:00', 7)
 
       expect(school.start_time).to eq('9:00')
     end
 
-    xit 'has hours in a school day' do
+    it 'has hours in a school day' do
       school = School.new('9:00', 7)
 
       expect(school.hours_in_school_day).to eq(7)
     end
 
-    xit 'starts with no student names' do
+    it 'starts with no student names' do
       school = School.new('9:00', 7)
 
       expect(school.student_names).to eq([])
@@ -30,7 +30,7 @@ RSpec.describe School do
   end
 
   context 'Iteration 2' do
-    xit 'can add student names' do
+    it 'can add student names' do
       school = School.new('9:00', 7)
 
       school.add_student_name('Aurora')
@@ -40,7 +40,7 @@ RSpec.describe School do
       expect(school.student_names).to eq(['Aurora', 'tim', 'megan'])
     end
 
-    xit 'can calculate end time' do
+    it 'can calculate end time' do
       school_1 = School.new('9:00', 7)
       school_2 = School.new('9:00', 3)
 
@@ -51,9 +51,22 @@ RSpec.describe School do
 
   context 'Iteration 3' do
     # Add your own tests that follow the interaction pattern for Iteration 3
+it 'checks for full time status' do
+  school = School.new('9:00', 7)
+
+  expect(school.is_full_time?).to eq(true)
   end
 
   context 'Iteration 4' do
     # Add your own tests that follow the interaction pattern for Iteration 4
+  it "can capitalize the first letter of each student's names" do
+    school = School.new('9:00', 7)
+
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+
+    expect.(school.standard_student_names).to eq (["Aurora", "Tim", "Megan"])
+    #=>
   end
 end
