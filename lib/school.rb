f9fab23e529d76be727b36c3f203882bb300c0db
phdd0 <phdd0.turing@gmail.com>
2022-02-02 19:50:57 -0700

    Add School class test pre-requisite

--- lib/school.rb
+++ lib/school.rb
@@ -1,21 +1,4 @@
 # ./lib/school.rb
 
-# class School
-#   def initialize(starts_at, day_len)
-#     @start = starts_at
-#     @day   = day_len
-#   end
-# end
-
 class School
-  def initialize(starts_at, day_len, add_student_name)
-    @start = starts_at
-    @day   = day_len
-    @
-  end
-
-  def add_student_name()
-    
-  end
-
 end
