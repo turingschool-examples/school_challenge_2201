5579a7141dc8037176ee9f04266440f2d83690d2
phdd0 <paul.haddick@outlook.com>
2022-03-12 09:48:49 -0700

    initc: pass uninit const School and attr_r from ip.it.1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,9 @@
-# ./lib/school.rb
+class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
+
+  def initialize(s,h)
+    @start_time = s
+    @hours_in_school_day = h 
+    @student_names = []
+  end
+end
